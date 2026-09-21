ee243079b78c3e02b63557c6784f9b5bce53eafa
engineerjesus717 <orlandomadrid717@gmail.com>
2026-09-21 14:49:22 -0700

    Add files via upload

---
+++ Homework_1_9.m
@@ -0,0 +1,24 @@
+%----------------------------- Problem 9 ---------------------------% 
+% 9. How large must a uniformly illuminated aperture antenna be 
+% (value of Dy) in terms of wavelengths so that its one-way 3-dB
+% beamwidth is 1°? What is the estimated gain in decibels of an 
+% antenna having azimuth and elevation beamwidths θ3 = φ3 = 1°,
+% based on the approximation in Eq. (1.11)?
+tic 
+clear; clc; close all; 
+myConstants;
+% 1. Antenna size determines beamwidth 
+% 2. Beamwidth determines approximate antenna gain 
+Omega_3dB = 1; 
+
+
+
+
+
+
+
+
+
+
+
+toc
